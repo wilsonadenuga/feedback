3ca27097075913642080c53bb364a1dadbea85af
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2025-12-19 20:20:08 +0100

    refactor(schema): organize response schemas into subdirectory
    - Move response schemas into response/ subdirectory
    - Add ApiResponse union type for better type inference
    - Consolidate exports through response/index.ts

--- packages/schema/src/common/index.ts
+++ packages/schema/src/common/index.ts
@@ -2,6 +2,4 @@ export * from './email.schema';
 export * from './uuid.schema';
 export * from './pagination.schema';
 export * from './date-range.schema';
-export * from './response.schema';
-export * from './success-response.schema';
-export * from './error-response.schema';
+export * from './response';
